f764481b61fa688daf6fe91389cdd49e02afc4b7
Steven Tran <stevenhuyt@gmail.com>
2023-07-31 14:14:37 -0400

    Allow program to get current user window

--- timer.py
+++ timer.py
@@ -1,7 +1,14 @@
 import pygetwindow as pgw
+import time
+def split_window_title(title):
+    title_parts = title.split(' - ')
+    return title_parts
 
 def user_active_window():
     active_window = pgw.getActiveWindow()
-    print(active_window.title)
-
+    title = active_window.title
+    title_list = split_window_title(title)
+    print(title_list[-1])
+    
 user_active_window()
+
