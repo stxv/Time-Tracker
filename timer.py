123fad5c657d06c08dd3cbdfb8945d8e39bc8bca
Steven Tran <stevenhuyt@gmail.com>
2023-07-31 14:54:42 -0400

    Allow program to determine what window the user is on and to make note of window changes.

--- timer.py
+++ timer.py
@@ -1,14 +1,20 @@
 import pygetwindow as pgw
 import time
+#Splits the title and puts each element into list by splitting at  " - " and gets the last element. (Normally looks like: timer.py - Time-Tracker - Visual Studio Code)
 def split_window_title(title):
     title_parts = title.split(' - ')
-    return title_parts
-
+    return title_parts[-1]
+#Function that gets active window, splits it, and returns it 
 def user_active_window():
-    active_window = pgw.getActiveWindow()
-    title = active_window.title
-    title_list = split_window_title(title)
-    print(title_list[-1])
-    
+    last_window = None
+    while True:
+        window = pgw.getActiveWindow()
+        title = window.title
+        active_window = split_window_title(title)
+#last_window starts as none, then active window is compared to last window (active window(anything) is definitely not equal to None)), print active then replace last window.
+        if active_window != last_window:
+            print(active_window)
+            last_window = active_window
+        time.sleep(1)
+        
 user_active_window()
-
