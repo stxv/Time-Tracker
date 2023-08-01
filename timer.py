4374220f472f52bb5aa8b9c3e486f6a099b0afd9
Steven Tran <stevenhuyt@gmail.com>
2023-08-01 10:20:44 -0400

    Not be perfect but gets the general idea. Able to recognize that users are on chrome and is switching between websites. Problem is that it gets title of the tab rather than website name

--- timer.py
+++ timer.py
@@ -4,15 +4,24 @@ import time
 def split_window_title(title):
     title_parts = title.split(' - ')
     return title_parts[-1]
+def google_title_split(title):
+    title_parts = title.split(' - ')
+    return title_parts
 #Function that gets active window, splits it, and returns it 
 def user_active_window():
     last_window = None
+    last_google_tab = None
     while True:
         try:
             window = pgw.getActiveWindow()
             if window is not None:
                 title = window.title
                 active_window = split_window_title(title)
+#
+                if active_window == "Google Chrome":
+                    if google_title_split(title) != last_google_tab:
+                        print(google_title_split(title))
+                        last_google_tab = google_title_split(title)
 #last_window starts as none, then active window is compared to last window (active window(anything) is definitely not equal to None)), print active then replace last window.
                 if active_window == "Task Switching":
                     continue
@@ -23,3 +32,4 @@ def user_active_window():
         except pgw.PyGetWindowException:
             pass
 user_active_window()
+# How the code is now, 
